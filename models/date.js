a91d1755cb2233f698760a43da23ac0c3bf20ae6
Chandan <chndan736@gmail.com>
2022-05-20 15:23:00 +0530

    deleted unwanted files

--- models/date.js
+++
@@ -1,8 +0,0 @@
-exports.currdate=function(){
-  let date = new Date();
-  return date.toLocaleDateString('en-GB');
-}
-exports.currtime=function(){
-  let date = new Date();
-  return date.toLocaleTimeString('en-US');
-}
